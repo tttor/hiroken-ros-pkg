3e3f1163a874acdf1e73d69cc62397d7d4be8a93
tor <vektor.dewanto@gmail.com>
2013-07-02 19:46:21 +0900

    put try-catch block when casting using boost::lexical_cast() plus some controls when a sample is damaged

--- learning_machine/include/data.hpp
+++ learning_machine/include/data.hpp
@@ -173,16 +173,40 @@ convert_csv2libsvmdata(const std::string& csv_path,const std::string& libsvmdata
       if(str_vals.at(0).size() == 0)
       {
         cerr << "str_vals.at(0).size() == 0 [csv is CORRUPTED]" << endl;
-        return false;
+        continue;
       }
       
       // Obtain the input vector and the output
       Input in;
+      bool all_in_okay = true;
       for(vector<string>::const_iterator i=str_vals.begin(); i!=str_vals.end()-1; ++i)// minus the single output value
-        in.push_back( boost::lexical_cast<double>(*i) );
-        
+      {
+        double in_val;
+        try
+        {
+          in_val = boost::lexical_cast<double>(*i);
+        }
+        catch(std::exception const&  ex)
+        {
+          std::cerr << ex.what() << std::endl;
+          all_in_okay = false;
+          break;
+        }
+      
+        in.push_back(in_val);
+      }
+      if(!all_in_okay) continue;
+      
       Output out;
-      out = boost::lexical_cast<double>(str_vals.back());
+      try
+      {
+        out = boost::lexical_cast<double>(str_vals.back());
+      }
+      catch(std::exception const&  ex)
+      {
+        std::cerr << ex.what() << std::endl;
+        continue;
+      }
       
       // Wrap up
       bool unique;
